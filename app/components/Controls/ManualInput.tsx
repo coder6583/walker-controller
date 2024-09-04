ecfdb9075a6f409a7d52af63736c5d32284e59b6
coder6583 <thedude6583@gmail.com>
2024-09-04 17:09:34 -0400

    input directly

--- app/components/Controls/ManualInput.tsx
+++ app/components/Controls/ManualInput.tsx
@@ -6,6 +6,7 @@ import {
   FormEventHandler,
   MouseEventHandler,
   SetStateAction,
+  useState,
 } from "react";
 
 export default function ManualInput({
@@ -21,20 +22,7 @@ export default function ManualInput({
   value: number;
   setValue: Dispatch<SetStateAction<number>>;
 }): JSX.Element {
-  const handleKeyInput: FormEventHandler<HTMLDivElement> = (e) => {
-    e.preventDefault();
-    if (e.currentTarget.textContent?.match(/[0-9\.]+/)) {
-      const num = Number(e.currentTarget.textContent);
-      if (id.includes("Voltage")) {
-        if (0 <= num && num <= 3.3) {
-          setValue(num);
-        }
-      }
-      if (id.includes("Delay")) {
-        setValue(num);
-      }
-    }
-  };
+  const [cursor, setCursor] = useState(0);
   const handlePlus: MouseEventHandler<HTMLDivElement> = () => {
     if (id.includes("Voltage")) {
       if (value < 3.3) {
@@ -69,11 +57,69 @@ export default function ManualInput({
         <FaPlus />
       </div>
       <div className={styles.number}>
-        <div className={styles.value} onInput={handleKeyInput}>
+        {/* <div className={styles.value} onInput={handleKeyInput}>
           {id.includes("Voltage")
             ? value.toPrecision(3).substring(0, 4)
             : value}
-        </div>
+        </div> */}
+        <input
+          className={styles.value}
+          value={
+            id.includes("Voltage")
+              ? value.toPrecision(3).substring(0, 4)
+              : " " + value
+          }
+          onKeyDown={(event) => {
+            console.log(event.key);
+            if (event.key === "Backspace") {
+              setValue(0.0);
+              setCursor(0);
+            } else if (!event.key.match(/[0-9\.]/)) {
+              event.preventDefault();
+            }
+          }}
+          onChange={(event) => {
+            if (
+              event.currentTarget.value.length <= 3 &&
+              event.currentTarget.value.match(/[0-9]+/)
+            ) {
+              setValue(Number(event.currentTarget.value));
+            } else if (
+              id.includes("Voltage") &&
+              event.currentTarget.value.match(/^[0-9].[0-9][0-9][0-9]$/)
+            ) {
+              if (cursor === 0) {
+                if (Number(event.currentTarget.value[4]) <= 3) {
+                  setValue(Number(event.currentTarget.value[4]));
+                  setCursor(cursor + 1);
+                }
+              } else if (cursor === 1) {
+                if (
+                  Number(event.currentTarget.value[0]) === 3 &&
+                  Number(event.currentTarget.value[4]) > 3
+                ) {
+                  return;
+                }
+                setValue(
+                  Number(
+                    Number(event.currentTarget.value[0]) +
+                      Number(event.currentTarget.value[4]) * 0.1
+                  )
+                );
+                setCursor(cursor + 1);
+              } else if (cursor === 2) {
+                setValue(
+                  Number(
+                    Number(event.currentTarget.value[0]) +
+                      Number(event.currentTarget.value[2]) * 0.1 +
+                      Number(event.currentTarget.value[4]) * 0.01
+                  )
+                );
+                setCursor(0);
+              }
+            }
+          }}
+        />
         <div className={styles.unit}>{unit}</div>
       </div>
       <div className={styles.button} onClick={handleMinus}>
